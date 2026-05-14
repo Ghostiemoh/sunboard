38890ee475520d3d0c8bfe83b8743cd3de919c9e
Muhammad <muhammad@example.com>
2026-05-14 23:04:25 +0100

    chore: isolate sunboard - untracking redundant sub-projects

--- marketing-video/probe_durations.ps1
+++
@@ -1,8 +0,0 @@
-$shell = New-Object -ComObject Shell.Application
-$folder = $shell.NameSpace('C:\Users\Muhammad\Documents\AntiGravity\marketing-video\public\raw_pitch')
-foreach ($item in $folder.Items()) {
-    if ($item.Name -like "*.MOV") {
-        $duration = $folder.GetDetailsOf($item, 27)
-        "$($item.Name): $duration" | Out-File -Append out.txt
-    }
-}
